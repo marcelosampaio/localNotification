1662ce53c3aea536af0c1ea6940d7d7de283e706
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-03-29 15:05:52 -0300

    Initial Commit

---
+++ localNotificationTests/localNotificationTests.m
@@ -0,0 +1,34 @@
+//
+//  localNotificationTests.m
+//  localNotificationTests
+//
+//  Created by Marcelo Sampaio on 3/29/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface localNotificationTests : XCTestCase
+
+@end
+
+@implementation localNotificationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
